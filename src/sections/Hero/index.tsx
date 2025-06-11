c48281e83598426b2f782082badb632748a8aa0c
Khalek <xalek.islam@gmail.com>
2025-06-11 23:42:54 +0300

    Hero: show once motion

--- src/sections/Hero/index.tsx
+++ src/sections/Hero/index.tsx
@@ -1,8 +1,8 @@
-import cn from "classnames";
-import { motion } from "motion/react";
+import cn from 'classnames';
+import { motion } from 'motion/react';
 
-import styles from "./Hero.module.scss";
-import { useAppContext } from "@/context/AppContext";
+import styles from './Hero.module.scss';
+import { useAppContext } from '@/context/AppContext';
 
 const Hero = () => {
   const { setShowForm } = useAppContext();
@@ -10,33 +10,36 @@ const Hero = () => {
   return (
     <section id="hero" className={styles.hero}>
       <motion.div
-        initial={{ transform: "scale(1)" }}
-        whileInView={{ transform: "scale(1.1)" }}
+        initial={{ transform: 'scale(1)' }}
+        whileInView={{ transform: 'scale(1.1)' }}
+        viewport={{ once: true, amount: 0.5 }}
         transition={{ delay: 0.3, duration: 1 }}
         className={styles.background}
       ></motion.div>
-      <div className={cn("container", styles.container)}>
+      <div className={cn('container', styles.container)}>
         <motion.h1
-          initial={{ opacity: 0, transform: "translateY(10px)" }}
-          whileInView={{ opacity: 1, transform: "translateY(0)" }}
+          initial={{ opacity: 0, transform: 'translateY(10px)' }}
+          whileInView={{ opacity: 1, transform: 'translateY(0)' }}
+          viewport={{ once: true, amount: 0.5 }}
           transition={{ delay: 0.2, duration: 1 }}
           className={styles.title}
         >
           Engineering Resilience for a Complex World
         </motion.h1>
         <motion.p
-          initial={{ opacity: 0, transform: "translateY(10px)" }}
-          whileInView={{ opacity: 1, transform: "translateY(0)" }}
+          initial={{ opacity: 0, transform: 'translateY(10px)' }}
+          whileInView={{ opacity: 1, transform: 'translateY(0)' }}
+          viewport={{ once: true, amount: 0.5 }}
           transition={{ delay: 0.3, duration: 1 }}
           className={styles.description}
         >
-          Global technology solutions built for national security, critical
-          infrastructure, and demanding environments.
+          Global technology solutions built for national security, critical infrastructure, and demanding environments.
         </motion.p>
 
         <motion.button
-          initial={{ opacity: 0, transform: "translateY(10px)" }}
-          whileInView={{ opacity: 1, transform: "translateY(0)" }}
+          initial={{ opacity: 0, transform: 'translateY(10px)' }}
+          whileInView={{ opacity: 1, transform: 'translateY(0)' }}
+          viewport={{ once: true, amount: 0.5 }}
           transition={{ delay: 0.6, duration: 1 }}
           className={styles.button}
           onClick={() => setShowForm(true)}
